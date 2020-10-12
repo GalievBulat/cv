00b5fbe9eb8aa10d93a2b491d1d654fa569f93d2
Your Name <kakadurf@gmail.com>
2020-10-12 08:22:03 +0300

    upd, del fix

--- src/main/java/Service/ConnectionHandler.java
+++ src/main/java/Service/ConnectionHandler.java
@@ -5,19 +5,24 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionHandler {
-    private static String url = "jdbc:postgresql://localhost:5432/postgres/usertc";
+    private static String url = "jdbc:postgresql://localhost:5432/postgres";
     private static String username = "postgres";
-    private static String password = "**";
+    private static String password = "";
 
     private static Connection connection = null;
+    private ConnectionHandler(){
+    }
 
-    public static Connection getConnection() throws SQLException {
-        try {
-            Class.forName("org.postgresql.Driver");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
+    public static synchronized Connection getConnection() throws SQLException {
+        if (connection==null) {
+            try {
+                Class.forName("org.postgresql.Driver");
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+            connection = DriverManager.getConnection(url, username, password);
+
         }
-        connection = DriverManager.getConnection(url, username, password);
         return connection;
     }
 
